d6d9d30f1507c5f0fd03020e1bb146971e131418
Sy Truong <sy.truong@trackinno.com>
2022-11-26 14:50:46 +0200

    add 1 more project, minor fix

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -4,7 +4,7 @@ import { sanityClient } from '../../sanity'
 import { PageInfo } from '../../typings'
 
 const query = groq`
-  *[_type == 'pageInfo']
+  *[_type == 'pageInfo'][0]
 `
 
 type Data = {
@@ -15,6 +15,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const pageInfos: PageInfo[] = await sanityClient.fetch(query)
-  res.status(200).json({ pageInfo: pageInfos[0] })
+  const pageInfo: PageInfo = await sanityClient.fetch(query)
+
+  res.status(200).json({ pageInfo })
 }
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -6,7 +6,5 @@ export const fetchPageInfo = async () => {
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
 
-    console.log(`PageInfo: ${pageInfo}`)
-
     return pageInfo;
 }
